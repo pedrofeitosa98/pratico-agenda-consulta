e33cd2642111045d4166fc80f50dfc3edb151870
pedrofeitosa98 <pedrohfeitosa98@gmail.com>
2022-10-20 01:48:53 -0300

    feature: components

---
+++ src/components/LeftSection/index.tsx
@@ -0,0 +1,33 @@
+import Logo from "../../components/Logo";
+import cliente from "../../assets/how-works-removebg-preview.png";
+import LeftSectionStyle from "./style";
+
+const LeftSection = () => {
+  return (
+    <LeftSectionStyle className="fadeInLeft">
+      <Logo />
+      <div className="text-div title2 color-white">
+        <table>
+          <tr>
+            <td className="text-right text-bold">1000+</td>
+            <td className="">clínicas</td>
+          </tr>
+          <tr>
+            <td className="text-righ text-bold">12.000+</td>
+            <td className="">laboratórios</td>
+          </tr>
+          <tr>
+            <td className="text-right text-bold">500+</td>
+            <td className="">farmácias</td>
+          </tr>
+        </table>
+        <p>
+          e com descontos pra <span className="text-bold">você!</span>
+        </p>
+      </div>
+      <img id="cliente" src={cliente} alt="" />
+    </LeftSectionStyle>
+  );
+};
+
+export default LeftSection;
---
+++ src/components/RightSection/FormLogin/index.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../Button/style";
+import { Input } from "../../Input/style";
+import Ruler from "../../Ruler";
+import RightContainer from "../style";
+
+const FormLogin = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <RightContainer className="fadeIn">
+        <h1 className="title1">Bem-vindo a Agenda Consulta!</h1>
+        <form action="">
+          <div>
+            <label htmlFor="">E-mail ou telefone</label>
+            <Input type="text" placeholder="Digite seu login" />
+          </div>
+
+          <div>
+            <label htmlFor="">Senha</label>
+            <Input type="password" placeholder="******" />
+          </div>
+
+          <div className="request-section-login">
+            <a href="/">
+              <p className="title4 color-black-1">Esqueceu sua senha?</p>
+            </a>
+            <Button filled className="title4 text-bold">
+              Fazer login
+            </Button>
+          </div>
+        </form>
+        <Ruler>ou</Ruler>
+        <div className="bottom-section">
+          <p className="title3">
+            Crie uma conta e faça parte de uma revolução da saúde.
+          </p>
+          <Button
+            filled={false}
+            className="title4 text-bold"
+            onClick={() => navigate("/register")}
+          >
+            Criar uma conta
+          </Button>
+        </div>
+      </RightContainer>
+    </>
+  );
+};
+
+export default FormLogin;
